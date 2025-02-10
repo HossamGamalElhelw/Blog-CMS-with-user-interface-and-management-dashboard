f7e16617a49bca7976e2d3c3791e75bd305c8637
hossam gamal <hossamgamal1761@gmail.com>
2025-02-11 00:05:44 +0200

    Updated files with Tailwind CSS changes

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className=''>
+        <div className=""></div>
+    
+    </div>
+  )
+}
+
+export default Dashboard
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import './Navbar.css'
 
 function Navbar() {
   return (
-    <div className='navbar_container'>
+    <div className='navbar_container flex justify-between items-center border-b-1 px-8 py-4'>
         <div className="logo">
-            <h3>Blog</h3>
+            <h3 className='text-secondary'>Blog</h3>
         </div>
-        <div className="user_login_info">
+        <div className="user_login_info flex items-center gap-4">
             <a href="#">Login</a>
             <a href="#">Singup</a>
             <p>hello,user</p>
